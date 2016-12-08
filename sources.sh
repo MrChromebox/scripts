c4e5840b60b97ea37986296983c365dab2ec0060
Matt DeVillier <matt.devillier@gmail.com>
2016-12-08 10:48:47 -0600

    12/08 firmware update
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- sources.sh
+++ sources.sh
@@ -3,14 +3,13 @@
 
 
 #define these here for easy updating
-script_date="[2016-11-27]"
+script_date="[2016-12-08]"
 
 #where the stuff is
 util_source="https://www.mrchromebox.tech/files/util/"
 rwlegacy_source="https://www.mrchromebox.tech/files/firmware/rw_legacy/"
 bootstub_source="https://www.mrchromebox.tech/files/firmware/boot_stub/"
 fullrom_source="https://www.mrchromebox.tech/files/firmware/full_rom/"
-fullrom_source_coolstar="https://dl.dropboxusercontent.com/u/59964215/chromebook/ROM/"
 shellball_source="https://www.mrchromebox.tech/files/firmware/shellball/"
 cbfs_source="https://www.mrchromebox.tech/files/firmware/cbfs/"
 
@@ -22,18 +21,18 @@ chrx_url="https://chrx.org/go"
 #LE version
 LE_version_base="LibreELEC-Generic.x86_64"
 LE_version_stable="7.0.2"
-LE_version_latest="7.90.008"
+LE_version_latest="7.90.09"
 
 #syslinux version
 syslinux_version="syslinux-6.04-pre1"
 
 #SBIB full ROMs - Legacy
-coreboot_stumpy="coreboot_seabios-stumpy-mrchromebox_20161107.rom"
+coreboot_stumpy="coreboot_seabios-stumpy-mrchromebox_20161208.rom"
 coreboot_parrot="coreboot_seabios-parrot_snb-mrchromebox_20161127.rom"
 coreboot_parrot_ivb="coreboot_seabios-parrot_ivb-mrchromebox_20161127.rom"
 #SBIB full ROMs - UEFI
-coreboot_uefi_parrot="coreboot_seabios_duet-parrot_snb-mrchromebox_20161127.rom"
-coreboot_uefi_parrot_ivb="coreboot_seabios_duet-parrot_ivb-mrchromebox_20161127.rom"
+coreboot_uefi_parrot="coreboot_seabios_duet-parrot_snb-mrchromebox_20161208.rom"
+coreboot_uefi_parrot_ivb="coreboot_seabios_duet-parrot_ivb-mrchromebox_20161208.rom"
 
 #Haswell full ROMs - Legacy
 coreboot_falco="coreboot_seabios-falco-mrchromebox_20161107.rom"
@@ -44,16 +43,16 @@ coreboot_peppy="coreboot_seabios-peppy-mrchromebox_20161107.rom"
 coreboot_peppy_elan="coreboot_seabios-peppy_elan-mrchromebox_20161107.rom"
 coreboot_wolf="coreboot_seabios-wolf-mrchromebox_20161107.rom"
 #Haswell full ROMs - UEFI
-coreboot_uefi_falco="coreboot_seabios_duet-falco-mrchromebox_20161127.rom"
-coreboot_uefi_leon="coreboot_seabios_duet-leon-mrchromebox_20161127.rom"
-coreboot_uefi_mccloud="coreboot_seabios_duet-mccloud-mrchromebox_20161127.rom"
-coreboot_uefi_monroe="coreboot_seabios_duet-monroe-mrchromebox_20161127.rom"
-coreboot_uefi_panther="coreboot_seabios_duet-panther-mrchromebox_20161127.rom"
-coreboot_uefi_peppy="coreboot_seabios_duet-peppy-mrchromebox_20161127.rom"
-coreboot_uefi_peppy_elan="coreboot_seabios_duet-peppy_elan-mrchromebox_20161127.rom"
-coreboot_uefi_tricky="coreboot_seabios_duet-tricky-mrchromebox_20161127.rom"
-coreboot_uefi_wolf="coreboot_seabios_duet-wolf-mrchromebox_20161127.rom"
-coreboot_uefi_zako="coreboot_seabios_duet-zako-mrchromebox_20161127.rom"
+coreboot_uefi_falco="coreboot_seabios_duet-falco-mrchromebox_20161208.rom"
+coreboot_uefi_leon="coreboot_seabios_duet-leon-mrchromebox_20161208.rom"
+coreboot_uefi_mccloud="coreboot_seabios_duet-mccloud-mrchromebox_20161208.rom"
+coreboot_uefi_monroe="coreboot_seabios_duet-monroe-mrchromebox_20161208.rom"
+coreboot_uefi_panther="coreboot_seabios_duet-panther-mrchromebox_20161208.rom"
+coreboot_uefi_peppy="coreboot_seabios_duet-peppy-mrchromebox_20161208.rom"
+coreboot_uefi_peppy_elan="coreboot_seabios_duet-peppy_elan-mrchromebox_20161208.rom"
+coreboot_uefi_tricky="coreboot_seabios_duet-tricky-mrchromebox_20161208.rom"
+coreboot_uefi_wolf="coreboot_seabios_duet-wolf-mrchromebox_20161208.rom"
+coreboot_uefi_zako="coreboot_seabios_duet-zako-mrchromebox_20161208.rom"
 
 #Broadwell full ROMs - Legacy
 coreboot_auron_paine="coreboot_seabios-auron_paine-mrchromebox_20161107.rom"
@@ -65,14 +64,14 @@ coreboot_rikku="coreboot_seabios-rikku-mrchromebox_20161107.rom"
 coreboot_samus="coreboot_seabios-samus-mrchromebox_20161107.rom"
 coreboot_tidus="coreboot_seabios-tidus-mrchromebox_20161107.rom"
 #Broadwell full ROMs - UEFI
-coreboot_uefi_auron_paine="coreboot_seabios_duet-auron_paine-mrchromebox_20161127.rom"
-coreboot_uefi_auron_yuna="coreboot_seabios_duet-auron_yuna-mrchromebox_20161127.rom"
-coreboot_uefi_gandof="coreboot_seabios_duet-gandof-mrchromebox_20161127.rom"
-coreboot_uefi_guado="coreboot_seabios_duet-guado-mrchromebox_20161127.rom"
-coreboot_uefi_lulu="coreboot_seabios_duet-lulu-mrchromebox_20161127.rom"
-coreboot_uefi_rikku="coreboot_seabios_duet-rikku-mrchromebox_20161127.rom"
-coreboot_uefi_samus="coreboot_seabios_duet-samus-mrchromebox_20161127.rom"
-coreboot_uefi_tidus="coreboot_seabios_duet-tidus-mrchromebox_20161127.rom"
+coreboot_uefi_auron_paine="coreboot_seabios_duet-auron_paine-mrchromebox_20161208.rom"
+coreboot_uefi_auron_yuna="coreboot_seabios_duet-auron_yuna-mrchromebox_20161208.rom"
+coreboot_uefi_gandof="coreboot_seabios_duet-gandof-mrchromebox_20161208.rom"
+coreboot_uefi_guado="coreboot_seabios_duet-guado-mrchromebox_20161208.rom"
+coreboot_uefi_lulu="coreboot_seabios_duet-lulu-mrchromebox_20161208.rom"
+coreboot_uefi_rikku="coreboot_seabios_duet-rikku-mrchromebox_20161208.rom"
+coreboot_uefi_samus="coreboot_seabios_duet-samus-mrchromebox_20161208.rom"
+coreboot_uefi_tidus="coreboot_seabios_duet-tidus-mrchromebox_20161208.rom"
 
 #BayTrail full ROMs - Legacy
 coreboot_candy="coreboot_seabios-candy-mrchromebox_20161107.rom"
