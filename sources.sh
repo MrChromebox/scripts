44d0439e231ee0dee613ae7b904209f824ac6c92
MrChromebox <matt.devillier@gmail.com>
2017-05-26 15:02:27 -0500

    Update to LE 8.0.2 stable (Kodi 17.3)

--- sources.sh
+++ sources.sh
@@ -20,8 +20,8 @@ chrx_url="https://chrx.org/go"
 
 #LE version
 LE_version_base="LibreELEC-Generic.x86_64"
-LE_version_stable="8.0.1"
-LE_version_latest="8.0.1"
+LE_version_stable="8.0.2"
+LE_version_latest="8.0.2"
 
 #syslinux version
 syslinux_version="syslinux-6.04-pre1"
