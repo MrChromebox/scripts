17f91e4c42ea86722600833ee864d7eb80addaa2
Matt DeVillier <matt.devillier@gmail.com>
2022-04-24 20:15:45 -0500

    functions: add shellball for babytiger
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- functions.sh
+++ functions.sh
@@ -97,7 +97,7 @@ UEFI_ROMS=($(printf "%s " "${hsw_boxes[@]}" "${hsw_books[@]}" "${bdw_boxes[@]}" 
 shellballs=($(printf "%s " "${hsw_boxes[@]}" "${hsw_books[@]}" "${bdw_boxes[@]}" \
     "${bdw_books[@]}" "${baytrail[@]}" "${snb_ivb[@]}" "${braswell[@]}" \
     "${skylake[@]}" 'atlas' 'eve' 'nautilus' 'nocturne' 'pantheon' 'sona' 'soraka' \
-	'teemo' 'sion' 'vayne' 'careena' 'liara' 'akemi' 'kohaku' 'barla' ))
+	'teemo' 'sion' 'vayne' 'careena' 'liara' 'akemi' 'kohaku' 'barla' 'babytiger'))
 runs_windows=($(printf "%s " "${snb_ivb[@]}" "${hsw_boxes[@]}" "${hsw_books[@]}" \
     "${bdw_boxes[@]}" "${bdw_books[@]}" "${baytrail[@]}" "${braswell[@]}" 'eve' \
     "${purism[@]}" "${kbl_boxes[@]}" "${cml_boxes[@]}"))
