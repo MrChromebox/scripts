07bdece9d57b2cde91c3e5c4c5630a585c5e9f76
Matt DeVillier <matt.devillier@gmail.com>
2023-01-10 14:17:17 -0600

    Update GLK RWL to default boot external sources
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- sources.sh
+++ sources.sh
@@ -3,7 +3,7 @@
 
 
 #define these here for easy updating
-export script_date="[2022-11-29]"
+export script_date="[2023-01-10]"
 
 #where the stuff is
 export util_source="https://www.mrchromebox.tech/files/util/"
@@ -212,7 +212,7 @@ export rwl_altfw_stoney="rwl_altfw_stoney-mrchromebox_20200107.bin"
 export rwl_altfw_whl="rwl_altfw_whl-mrchromebox_20201017.bin"
 export rwl_altfw_cml="rwl_altfw_cml-mrchromebox_20210415.bin"
 export rwl_altfw_drallion="rwl_altfw_drallion-mrchromebox_20221019.bin"
-export rwl_altfw_glk="rwl_altfw_glk-mrchromebox_20221019.bin"
+export rwl_altfw_glk="rwl_altfw_glk-mrchromebox_20230110.bin"
 export rwl_altfw_jsl="rwl_altfw_jsl-mrchromebox_20211115.bin"
 export rwl_altfw_zen2="rwl_altfw_zen2-mrchromebox_20210623.bin"
 export rwl_altfw_tgl="rwl_altfw_tgl-mrchromebox_20210827.bin"
