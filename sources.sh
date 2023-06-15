657e416e22a7756571cca07b3789d3b29fa0e7bf
Matt DeVillier <matt.devillier@gmail.com>
2023-06-15 13:12:47 -0500

    sources: update REDRIX image
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- sources.sh
+++ sources.sh
@@ -3,7 +3,7 @@
 
 
 #define these here for easy updating
-export script_date="[2023-05-23]"
+export script_date="[2023-06-15]"
 
 #where the stuff is
 export util_source="https://www.mrchromebox.tech/files/util/"
@@ -248,7 +248,7 @@ export coreboot_uefi_kano="coreboot_edk2-kano-mrchromebox_20230515.rom"
 export coreboot_uefi_mithrax="coreboot_edk2-mithrax-mrchromebox_20230515.rom"
 export coreboot_uefi_osiris="coreboot_edk2-osiris-mrchromebox_20230515.rom"
 export coreboot_uefi_primus="coreboot_edk2-primus-mrchromebox_20230515.rom"
-export coreboot_uefi_redrix="coreboot_edk2-redrix-mrchromebox_20230515.rom"
+export coreboot_uefi_redrix="coreboot_edk2-redrix-mrchromebox_20230615.rom"
 export coreboot_uefi_taniks="coreboot_edk2-taniks-mrchromebox_20230523.rom"
 export coreboot_uefi_volmar="coreboot_edk2-volmar-mrchromebox_20230515.rom"
 export coreboot_uefi_zavala="coreboot_edk2-zavala-mrchromebox_20230515.rom"
