0448fe40d3a82c015f8e6f66a371fa464a9c6515
Matt DeVillier <matt.devillier@gmail.com>
2024-01-07 20:54:54 -0600

    sources: update PCO images
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- sources.sh
+++ sources.sh
@@ -3,7 +3,7 @@
 
 
 #define these here for easy updating
-export script_date="[2023-12-25]"
+export script_date="[2024-01-07]"
 
 #where the stuff is
 export util_source="https://www.mrchromebox.tech/files/util/"
@@ -293,15 +293,15 @@ export coreboot_uefi_liara="coreboot_edk2-liara-mrchromebox_20231222.rom"
 export coreboot_uefi_treeya="coreboot_edk2-treeya-mrchromebox_20231222.rom"
 
 #Picasso
-export coreboot_uefi_berknip="coreboot_edk2-berknip-mrchromebox_20231225.rom"
-export coreboot_uefi_dirinboz="coreboot_edk2-dirinboz-mrchromebox_20231225.rom"
-export coreboot_uefi_ezkinil="coreboot_edk2-ezkinil-mrchromebox_20231225.rom"
-export coreboot_uefi_gumboz="coreboot_edk2-gumboz-mrchromebox_20231225.rom"
-export coreboot_uefi_jelboz360="coreboot_edk2-jelboz360-mrchromebox_20231225.rom"
-export coreboot_uefi_morphius="coreboot_edk2-morphius-mrchromebox_20231225.rom"
-export coreboot_uefi_morphius_tp="coreboot_edk2-morphius_tp-mrchromebox_20231225.rom"
-export coreboot_uefi_vilboz="coreboot_edk2-vilboz-mrchromebox_20231225.rom"
-export coreboot_uefi_woomax="coreboot_edk2-woomax-mrchromebox_20231225.rom"
+export coreboot_uefi_berknip="coreboot_edk2-berknip-mrchromebox_20240107.rom"
+export coreboot_uefi_dirinboz="coreboot_edk2-dirinboz-mrchromebox_20240107.rom"
+export coreboot_uefi_ezkinil="coreboot_edk2-ezkinil-mrchromebox_20240107.rom"
+export coreboot_uefi_gumboz="coreboot_edk2-gumboz-mrchromebox_20240107.rom"
+export coreboot_uefi_jelboz360="coreboot_edk2-jelboz360-mrchromebox_20240107.rom"
+export coreboot_uefi_morphius="coreboot_edk2-morphius-mrchromebox_20240107.rom"
+export coreboot_uefi_morphius_tp="coreboot_edk2-morphius_tp-mrchromebox_20240107.rom"
+export coreboot_uefi_vilboz="coreboot_edk2-vilboz-mrchromebox_20240107.rom"
+export coreboot_uefi_woomax="coreboot_edk2-woomax-mrchromebox_20240107.rom"
 
 #Cezanne
 export coreboot_uefi_dewatt="coreboot_edk2-dewatt-mrchromebox_20231222.rom"
