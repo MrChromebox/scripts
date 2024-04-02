86442f844a245af73b4b15fab38ab3876008feea
Matt DeVillier <matt.devillier@gmail.com>
2024-04-02 15:33:13 -0500

    Fix detection for PUJJOTEEN15W
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- functions.sh
+++ functions.sh
@@ -777,7 +777,8 @@ case "${_hwid}" in
 	PUJJOFLEX*)             _x='ADN|Lenovo IdeaPad Flex 3i Chromebook'; device="pujjoflex" ;;
 	PUJJOTEEN*-CZPM* | PUJJOTEEN*-JQLW*)
 	                        _x='ADN|Lenovo 14e Chromebook Gen 3'; device="pujjoteen" ;;
-	PUJJOTEEN*-KCBW*)       _x='ADN|Lenovo Ideapad Slim 3i Chromebook'; device="pujjoteen15w" ;;
+	PUJJOTEEN*-KCBW* | PUJJOTEEN15W)
+	                        _x='ADN|Lenovo Ideapad Slim 3i Chromebook'; device="pujjoteen15w" ;;
 	PUJJO-DCCV*)            _x='ADN|Lenovo Flex 3i Chromebook 12"'; device="pujjo" ;;
 	PUJJO-KTLR*)            _x='ADN|Lenovo 500e Yoga Chromebook Gen 4'; device="pujjo" ;;
 	PYRO*)                  _x='APL|Lenovo Thinkpad 11e/Yoga Chromebook (G4)' ;;
