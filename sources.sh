e422e1b95083f678911da91bb113bd0699568100
Matt DeVillier <matt.devillier@gmail.com>
2024-11-01 10:52:24 -0500

    sources: update KARMA/ENDEAVOUR images w/USB port fix
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- sources.sh
+++ sources.sh
@@ -3,7 +3,7 @@
 
 
 #define these here for easy updating
-export script_date="[2024-09-14]"
+export script_date="[2024-11-01]"
 
 #where the stuff is
 export util_source="https://www.mrchromebox.tech/files/util/"
@@ -84,10 +84,10 @@ export coreboot_uefi_sentry="coreboot_edk2-sentry-mrchromebox_20240914.rom"
 
 #KabyLake
 export coreboot_uefi_atlas="coreboot_edk2-atlas-mrchromebox_20240914.rom"
-export coreboot_uefi_endeavour="coreboot_edk2-endeavour-mrchromebox_20240914.rom"
+export coreboot_uefi_endeavour="coreboot_edk2-endeavour-mrchromebox_20241101.rom"
 export coreboot_uefi_eve="coreboot_edk2-eve-mrchromebox_20240914.rom"
 export coreboot_uefi_fizz="coreboot_edk2-fizz-mrchromebox_20240914.rom"
-export coreboot_uefi_karma="coreboot_edk2-karma-mrchromebox_20240914.rom"
+export coreboot_uefi_karma="coreboot_edk2-karma-mrchromebox_20241101.rom"
 export coreboot_uefi_leona="coreboot_edk2-leona-mrchromebox_20240914.rom"
 export coreboot_uefi_nami="coreboot_edk2-nami-mrchromebox_20240914.rom"
 export coreboot_uefi_nautilus="coreboot_edk2-nautilus-mrchromebox_20240914.rom"
