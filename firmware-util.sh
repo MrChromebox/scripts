e36199b474e535344e041be5c87602d937b27921
Matt DeVillier <matt.devillier@gmail.com>
2025-10-03 15:57:12 -0500

    Consolidate device info in new array/db struct
    Add functions to support it / integrate with existing code.
    Remove unused functions/variables. Minor cleanup.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- firmware-util.sh
+++ firmware-util.sh
@@ -11,8 +11,10 @@
 # as long as proper attribution is given.
 #
 
+# shellcheck disable=SC2164
+
 #path to directory where script is saved
-script_dir="$(dirname $(readlink -f $0))"
+script_dir="$(dirname "$(readlink -f "$0")")"
 
 #where the stuff is
 script_url="https://raw.githubusercontent.com/MrChromebox/scripts/main/"
@@ -23,10 +25,9 @@ export LC_ALL=C
 #set working dir
 if grep -q "Chrom" /etc/lsb-release; then
 	# needed for ChromeOS/ChromiumOS v82+
-	mkdir -p /usr/local/bin
-	cd /usr/local/bin
+	mkdir -p /usr/local/bin && cd /usr/local/bin
 else
-	cd /tmp
+	mkdir -p /tmp && cd /tmp
 fi
 
 # clear screen / show banner
@@ -42,26 +43,33 @@ fi
 if [ ! -d "$script_dir/.git" ]; then
 	script_dir="."
 
+	# Array of required files to download
+	required_files=(
+		"device-db.sh"
+		"device-db-functions.sh"
+		"firmware.sh"
+		"functions.sh"
+		"sources.sh"
+	)
+
 	#get support scripts
 	echo -e "\nDownloading supporting files..."
-	rm -rf firmware.sh >/dev/null 2>&1
-	rm -rf functions.sh >/dev/null 2>&1
-	rm -rf sources.sh >/dev/null 2>&1
-	$CURL -sLO ${script_url}firmware.sh
-	rc0=$?
-	$CURL -sLO ${script_url}functions.sh
-	rc1=$?
-	$CURL -sLO ${script_url}sources.sh
-	rc2=$?
-	if [[ $rc0 -ne 0 || $rc1 -ne 0 || $rc2 -ne 0 ]]; then
-		echo -e "Error downloading one or more required files; cannot continue"
-		exit 1
-	fi
+
+	for file in "${required_files[@]}"; do
+		rm -rf "${file}" >/dev/null 2>&1
+		if ! $CURL -sLO "${script_url}${file}"; then
+			echo -e "\nError downloading $file; cannot continue"
+			exit 1
+		fi
+	done
+
 fi
 
-source $script_dir/sources.sh
-source $script_dir/firmware.sh
-source $script_dir/functions.sh
+source "$script_dir/device-db.sh"
+source "$script_dir/device-db-functions.sh"
+source "$script_dir/sources.sh"
+source "$script_dir/firmware.sh"
+source "$script_dir/functions.sh"
 
 #set working dir
 cd /tmp
